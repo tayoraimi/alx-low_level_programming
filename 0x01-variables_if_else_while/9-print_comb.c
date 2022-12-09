55539e96bbcaa1e595b63326f7215516a8f9703c
Temitayo <tayo.raimi@gmail.com>
2022-12-09 06:47:45 -0800

    var9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char num;
+
+	for (num ='0'; num <= '9'; num++)
+	{
+		putchar(num);
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
