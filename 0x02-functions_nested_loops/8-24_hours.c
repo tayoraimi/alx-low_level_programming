a7a18b475fd13fb93b6602be176aa46298b36b06
Temitayo <tayo.raimi@gmail.com>
2022-12-13 10:19:26 -0800

    functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- *
  * jack_bauer - prints every minute of the day
  *
  * Return: no return
@@ -13,20 +12,20 @@ void jack_bauer(void)
 	{
 		for (b = 48; b <= 57; b++)
 		{
-		       for (c = 48; c <= 53; c++)
-		       {
-			       for (d = 48; d <= 57; d++)
-			       {
-				      if (a >= 50 && b >= 52)
-					      break;
-				      _putchar(a);
-				      _putchar(b);
-				      _putchar(58);
-				      _putchar(c);
-				      _putchar(d);
-				      _putchar('\n');
-			       }
-		       }
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					if (a >= 50 && b >= 52)
+						break;
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
 		}
 	}
 }
