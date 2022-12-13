7df9fada6cc61759063b3175a0dbe7df60478f06
Temitayo <tayo.raimi@gmail.com>
2022-12-13 09:26:04 -0800

    functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * pprint_alphabet - Print the alphabet in lowercase.
+ * print_alphabet - Print the alphabet in lowercase.
  *
  *  Return: void.
  */
-
 void print_alphabet(void)
 {
 	int i;
+	
 	for (i = 97; i < 123; i++)
 	{
 		_putchar(i);
