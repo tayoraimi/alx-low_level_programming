c87ff3ff3b770688d1f9a8d58139e56449b8d45b
Temitayo <tayo.raimi@gmail.com>
2022-12-19 01:43:24 -0800

    pointers

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,9 +13,17 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	char *aux = dest;
+	int l = 0;
+	int x = 0;
 	
-	while (*src)
-		*dest++ = *src++;
-	return (aux);
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
 }
