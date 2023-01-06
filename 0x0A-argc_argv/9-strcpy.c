dd9660a7fabf0d7c44fc14f960665e9c3698e4a9
Temitayo <tayo.raimi@gmail.com>
2023-01-06 03:51:34 -0800

    0x0a

---
+++ 0x0A-argc_argv/9-strcpy.c
@@ -0,0 +1,29 @@
+/**
+ * _strcpy - a function that copies the string pointed to by src,
+ *
+ * including the terminating null byte (\0),
+ *
+ * to the buffer pointed to by dest.
+ *
+ * @dest: A pointer to destination of string
+ *
+ * @src: A pointer to source string to copy from
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+	
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
