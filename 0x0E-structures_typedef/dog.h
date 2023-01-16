79a6be4f1478557d61d7f8bf9ef41982b3d41c13
Temitayo <tayo.raimi@gmail.com>
2023-01-16 03:32:50 -0800

    0x0D

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
  * struct dog - structure for a basic dog
  *
@@ -10,7 +12,6 @@
  * Description: data structure for dog
  */
 struct dog{
-
 	char *name;
 	float owner;
 	char *age;
@@ -21,3 +22,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif
